e38e77d6da53430b048a16428508be8815f54759
Matt Gibbs <mgibbs@gatech.edu>
2015-06-16 16:27:50 -0700

    Better handle client connections and messages.

--- klystronserver.py
+++ klystronserver.py
@@ -17,39 +17,46 @@ logger.addHandler(filehandler)
 logger.setLevel(logging.DEBUG)
 app = Bottle()
 
+klystrons = {}
+initialized_clients = set()
+initialized = False
+
 class KlystronServerApplication(WebSocketApplication):
 	def __init__(self, ws):
 		super(KlystronServerApplication, self).__init__(ws)
-		self.klystrons = {}
+		if not initialized:
+			self.initialize_klystrons()
+		
+	def initialize_klystrons(self):
 		sectors = {}
 		sectors[20] = [5,6,7,8];
 		for s in range(21,31):
 			sectors[s] = range(0,9)
 		sectors[24] = [0,1,2,3,4,5,6,8]
-		
-		
 		for sector in sectors:
 			klystrons_in_sector = {}
 			for station in sectors[sector]:
 				if (sector == 24 and station == 7):
 					continue
 				klystrons_in_sector[station] = Klystron(sector,station,faults_callback=self.klystron_fault_callback, triggers_callback=self.klystron_triggers_callback)
-			self.klystrons[sector] = klystrons_in_sector
+			klystrons[sector] = klystrons_in_sector
 		logger.debug("Finished connecting to klystrons.")
-			
+		initialized = True
+	
 	def klystron_fault_callback(self, sector, station, faults):
-		for client in self.ws.handler.server.clients.values():
+		for client in initialized_clients:
 			client.ws.send(ujson.dumps({'sector': sector, 'station': station, 'faults': faults}))
 		
 	def klystron_triggers_callback(self, sector, station, trigger_status):
-		for client in self.ws.handler.server.clients.values():
+		for client in initialized_clients:
 			client.ws.send(ujson.dumps({'sector': sector, 'station': station, 'trigger_status': trigger_status}))
 			
 	def send_current_state(self, client):
-		for sector in self.klystrons:
-			for station in self.klystrons[sector]:
-				client.ws.send(ujson.dumps({'sector': sector, 'station': station, 'faults': self.klystrons[sector][station].faults, 'trigger_status': self.klystrons[sector][station].acc_trigger_status}))
-	
+		for sector in klystrons:
+			for station in klystrons[sector]:
+				client.ws.send(ujson.dumps({'sector': sector, 'station': station, 'faults': klystrons[sector][station].faults, 'trigger_status': klystrons[sector][station].acc_trigger_status}))
+		initialized_clients.add(client)
+		
 	def on_open(self):
 		current_client = self.ws.handler.active_client
 		logger.debug("Connection opened.")
@@ -60,6 +67,8 @@ class KlystronServerApplication(WebSocketApplication):
 		pass
 		
 	def on_close(self, reason):
+		current_client = self.ws.handler.active_client
+		initialized_clients.remove(current_client)
 		logger.debug("Connection closed.")
 		
 #@app.route('/<filename:path>')
